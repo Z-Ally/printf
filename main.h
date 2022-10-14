483d14f7d68201a5b8e8995112191ffa2d1fd644
akadwa <kadazh123@gmail.com>
2022-10-14 12:36:23 -0700

    Added header file main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+/**
+ * struct format - Structure for printing various types
+ *
+ */
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
